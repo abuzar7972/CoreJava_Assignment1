3fd810d2f54412b87e0eb5933ec363b14501d2fe
abuzar7972 <abushaikh7972@gmail.com>
2021-06-21 14:28:47 +0530

    Calculated Employee Wage Till Condition Reach

--- display.java
+++ display.java
@@ -1,28 +1,14 @@
 public class display{
 	public static void main(String[] argv){
-	int Is_PartTime_Present = 1;
-	int Is_FullTime_Present=2;
-	int Is_Absent=0;
+	int days=1;
+	int salary=0;
 	int EmpWagePerHours=20;
-	int EmpWorkingHours=0;
-	double i = Math.floor(Math.random() * 10) %3;
-	int empCheck = (int) i;
-		switch (empCheck)
+	int EmpWorkingHours=8;
+		while (days <= 20)
 		{
-		case 1:
-			System.out.println("Employee is Full time Present");
-			EmpWorkingHours=8;
-				break;
-		case 2:
-			System.out.println("Employee is Part time present");
-			EmpWorkingHours=4;
-				break;
-		case 0:
-			System.out.println("Employee is Absent");
-			EmpWorkingHours=0;
-				break;
+			salary += EmpWagePerHours*EmpWorkingHours;
+			days = days+1;
 		}
-		int salary = EmpWagePerHours*EmpWorkingHours*20;
 		System.out.println("Salary Per Month");
 		System.out.println(salary);
 }
