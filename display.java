e82b889f792485cbf86ff0371089d4d6c2519d96
abuzar7972 <abushaikh7972@gmail.com>
2021-06-21 13:16:44 +0530

    Employee Check

--- display.java
+++ display.java
@@ -1,5 +1,10 @@
 public class display{
 	public static void main(String[] argv){
-	System.out.println("hello");
+	int Is_Present = 1;
+	double empCheck = Math.floor(Math.random() * 10) %2;
+		if (empCheck == Is_Present)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Abset");
 }
 }
