8b6714689eb8cd17d4290d2dfccce02e94a971fe
abuzar7972 <abushaikh7972@gmail.com>
2021-06-21 13:24:48 +0530

    Calculated Employee Salary based on Presenty

--- display.java
+++ display.java
@@ -1,10 +1,20 @@
 public class display{
 	public static void main(String[] argv){
 	int Is_Present = 1;
+	int EmpWagePerHours=20;
+	int EmpWorkingHours=0;
 	double empCheck = Math.floor(Math.random() * 10) %2;
 		if (empCheck == Is_Present)
+		{
 			System.out.println("Employee is Present");
+			EmpWorkingHours=8;
+		}
 		else
+		{
 			System.out.println("Employee is Abset");
+			EmpWorkingHours=0;
+		}
+		int salary = EmpWagePerHours*EmpWorkingHours;
+		System.out.println(salary);
 }
 }
