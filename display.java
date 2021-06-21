e9863d158ff8b86e507601d6e183ae55cec14dd9
abuzar7972 <abushaikh7972@gmail.com>
2021-06-21 14:02:33 +0530

    Calculated Wages with CASE statement

--- display.java
+++ display.java
@@ -2,24 +2,25 @@ public class display{
 	public static void main(String[] argv){
 	int Is_PartTime_Present = 1;
 	int Is_FullTime_Present=2;
-	int Is_Absent=3;
+	int Is_Absent=0;
 	int EmpWagePerHours=20;
 	int EmpWorkingHours=0;
-	double empCheck = Math.floor(Math.random() * 10) %3;
-		if (empCheck == Is_FullTime_Present)
+	double i = Math.floor(Math.random() * 10) %3;
+	int empCheck = (int) i;
+		switch (empCheck)
 		{
+		case 1:
 			System.out.println("Employee is Full time Present");
 			EmpWorkingHours=8;
-		}
-		if ( empCheck == Is_PartTime_Present )
-		{
+				break;
+		case 2:
 			System.out.println("Employee is Part time present");
 			EmpWorkingHours=4;
-		}
-		if ( empCheck == Is_Absent)
-		{
+				break;
+		case 0:
 			System.out.println("Employee is Absent");
 			EmpWorkingHours=0;
+				break;
 		}
 		int salary = EmpWagePerHours*EmpWorkingHours;
 		System.out.println(salary);
